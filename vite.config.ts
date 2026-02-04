163e4759a267a3f7b5f10d48f611e1135f7148e0
znrfrnz <franzbaes69@gmail.com>
2026-02-05 04:21:40 +0800

    test using vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000", // Points to your new local server
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
